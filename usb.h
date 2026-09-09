60325d2098aaceb83370bef93109de0f24721728
FogboundSloth25 <108718835+FogboundSloth25@users.noreply.github.com>
2026-09-09 18:02:24 +0500

    Add usb.h header file with USB bus functions

---
+++ usb.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "bus.h"
+
+
+void usb_start(void);
+
+int usb_bus_init(void);
+
+int usb_bus_wait_for_device(void);
+
+int usb_bus_reset_open_ep0(void);
+
+
+typedef int (*usb_executee_t)(
+    bus_t *b,
+    void *ctx
+);
+
+
+int usb_bus_execute(
+    usb_executee_t func,
+    void *ctx,
+    uint64_t timeout
+);
+
+
+bool usb_bus_is_connected(void);
